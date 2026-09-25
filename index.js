1848daf9cce99e306c3f9ff75e16b6f88eb28841
Japhet Korir <japhet.korir@student.moringa.com>
2026-09-25 15:01:55 +0300

    Complete all JavaScript fundamental logic functions and pass tests

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
 
+function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * discountPercentage / 100);
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = {
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice
+};
